5fd0e6aaf3c504e8b7647026bb54d8b245db64e5
Zeff Morgan <zmorgan@lexmark.com>
2018-04-30 12:51:14 -0400

    Fahrenheit tests and conversions

---
+++ convert/to_fahrenheit.spec.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const toFahrenheit = require('./to_fahrenheit').toFahrenheit;
+const expect = require('chai').expect;
+
+describe('Conversion functions', () => {
+    describe('"toFahrenheit"', () => {
+        it('should be a function', () => {
+            expect(toFahrenheit).to.be.a('function');
+        }),
+        it('should return correct value when converting from Celsius', () => {
+            let args = { temp: 100, uom: 'c' };
+            expect(toFahrenheit(args)).to.equal(212);
+        }),
+        it('should return correct value when converting from Kelvin', () => {
+            let args = { temp: 200, uom: 'k' };
+            expect(toFahrenheit(args)).to.equal(-99.66999999999996);
+        }),
+        it('should return correct value when converting from Rankine', () => {
+            let args = { temp: 400, uom: 'r' };
+            expect(toFahrenheit(args)).to.equal(-59.670000000000016);
+        });
+    });
+});
